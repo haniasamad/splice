1d96b006ba28aa0c638c5e0e5726e931d86c6430
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-07-09 18:42:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var ques1 = prompt("where from the array you want to starting?")
+var ques2 = prompt("how many elements you want to delete?")
+var students = ["nimra" , "wasay" , "fatima" , "aniq" , "musa"]
+
+alert(students)
+
+students.splice(ques1,ques2,)
+console.log(students);
